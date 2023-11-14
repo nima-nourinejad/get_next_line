854e3629b2b38e0fa525896032458c0a4a944204
Nima Nourinejad <nnourine@c2r2p9.hive.fi>
2023-11-14 10:27:19 +0200

    the initial structure is created

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 11:36:16 by nnourine          #+#    #+#             */
+/*   Updated: 2023/11/14 10:23:43 by nnourine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <>
+
+char	*get_next_line(int fd);
+
+#endif // GET_NEXT_LINE_H
