a3b705a2af306aab1dabd5a1c09bc2fb1a334186
Nima Nourinejad <nnourine@c2r2p9.hive.fi>
2023-11-16 15:55:08 +0200

    passed the strict test

--- get_next_line.c
+++ get_next_line.c
@@ -6,44 +6,118 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 10:22:38 by nnourine          #+#    #+#             */
-/*   Updated: 2023/11/14 14:21:53 by nnourine         ###   ########.fr       */
+/*   Updated: 2023/11/16 15:24:12 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+//#include <stdio.h> //
+
+static char	*ufree(void *extra, void *buffer)
+{
+	if (extra)
+	{
+		free (extra);
+		extra = 0;
+	}
+	if (buffer)
+		free (buffer);
+	return (NULL);
+}
+
 
 char	*get_next_line(int fd)
 {
-	static char	extra[BUFFER_SIZE];
-	char		buffer[BUFFER_SIZE];
+	char		buffer[BUFFER_SIZE + 1];
+	static char	extra[BUFFER_SIZE + 1] = "";
 	char		*line;
+	char		*temp;
 	ssize_t		byt;
+	int 		i;
 
-	if (read(fd, beffer, 0) < 0)
-		return (NULL);
-	byt = read(fd, beffer, BUFFER_SIZE);
-	if (byt < 0)
-		return (NULL);
-	i = 0;
-	line = ft_strdup (extra);
-	while (byt > 0)
+	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
 	{
-		if (strchr(buffer, '\n') == 0)
+		i = 0;
+		while (i < BUFFER_SIZE + 1)
 		{
-			line = strjoin(line, buffer);
-			byt = read(fd, beffer, BUFFER_SIZE);
-			if (byt < 0)
-				return (NULL);
+			extra[i] = '\0';
+			i++;
 		}
-		else
+		return (NULL);
+	}
+	line = ft_strjoin(0, extra, 0);
+	if (!line)
+	{
+		i = 0;
+		while (i < BUFFER_SIZE + 1)
 		{
-			extra = strdup(strchr(buffer, '\n') + 1);
-			*strchr(buffer, '\n') = '\0';
-			line = strjoin(line, strdup(buffer));
-			line = strjoin(line, "\n");
-			return (line);
+			extra[i] = '\0';
+			i++;
+		}
+		return (ufree(0, 0));
+	}
+	i = 0;
+	while (i < BUFFER_SIZE + 1)
+	{
+		extra[i] = '\0';
+		i++;
+	}
+	byt = 1;
+	while (ft_strchr(line, '\n') == 0 && byt)
+	{
+		byt = read(fd, buffer, BUFFER_SIZE);
+		if (byt < 0)
+		{
+			//printf( "extra : %s\n",extra);
+			i = 0;
+			while (i < BUFFER_SIZE + 1)
+			{
+				extra[i] = 0;
+				i++;
+			}
+			//printf( "extra : %s\n",extra);
+			i = 0;
+			free(line);
+			return (NULL);
+		}
+		buffer[byt] = '\0';
+		temp = line;
+		line = ft_strjoin(line, buffer, 0);
+		free(temp);
+		if (!line)
+		{
+			i = 0;
+			while (i < BUFFER_SIZE + 1)
+			{
+				extra[i] = '\0';
+				i++;
+			}
+			return (ufree(0, 0));
 		}
 	}
-	line = strjoin(line, "\n");
+	if (byt)
+	{
+		if (*(ft_strchr(line, '\n') + 1) != '\0')
+		{
+			ft_memcpy(extra, (ft_strchr(line, '\n') + 1),
+				ft_strlen(ft_strchr(line, '\n') + 1));
+			*(ft_strchr(line, '\n') + 1) = '\0';
+			temp = line;
+			line = ft_strjoin(0, line, 0);
+			free(temp);
+			if (!line)
+			{
+				i = 0;
+				while (i < BUFFER_SIZE + 1)
+				{
+					extra[i] = '\0';
+					i++;
+				}
+				return (ufree(0, 0));
+			}
+		}
+	}
+	if (!line[0])
+		return (ufree(0, line));
 	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,17 +6,19 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 10:24:17 by nnourine          #+#    #+#             */
-/*   Updated: 2023/11/14 14:03:09 by nnourine         ###   ########.fr       */
+/*   Updated: 2023/11/16 10:53:27 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s)
+size_t	ft_strlen(char *s)
 {
 	size_t	len;
 
 	len = 0;
+	if (!s)
+		return (0);
 	if (*s == '\0')
 		return (0);
 	else
@@ -30,26 +32,51 @@ size_t	ft_strlen(const char *s)
 	}
 }
 
-char	*ft_strdup(const char *s)
+char	*ft_memcpy(char *d, char *s, size_t n)
 {
-	size_t	l;
-	char	*d;
+	size_t	i;
 
-	l = ft_strlen(s);
-	d = malloc((l + 1) * sizeof (char));
-	if (d == 0)
-		return (0);
-	ft_memcpy (d, s, l + 1);
+	i = 0;
+	if (n == 0)
+		return (d);
+	while (i < n)
+	{
+		*(d + i) = *(s + i);
+		i++;
+	}
 	return (d);
 }
 
-char	*ft_strchr(const char *s, int c1)
+char	*ft_strjoin(char *s1, char *s2, int triger)
+{
+	char	*d;
+	size_t	l1;
+	size_t	l2;
+
+	l1 = ft_strlen (s1);
+	l2 = ft_strlen (s2);
+	d = malloc((l1 + l2 + 1 + triger) * sizeof (char));
+	if (d == 0)
+		return (0);
+	ft_memcpy(d, s1, l1);
+	ft_memcpy(d + l1, s2, l2);
+	if (triger == 1)
+	{
+		d[l1 + l2] = '\n';
+		d[l1 + l2 + 1] = '\0';
+	}
+	else
+		d[l1 + l2] = '\0';
+	return (d);
+}
+
+char	*ft_strchr(char *s, char c)
 {
 	int		i;
-	char	c;
 
 	i = 0;
-	c = (char) c1;
+	if (!s)
+		return (0);
 	if (c == 0)
 		return ((char *)(s + ((int)ft_strlen(s))));
 	while (s[i] != '\0')
@@ -59,46 +86,4 @@ char	*ft_strchr(const char *s, int c1)
 		i++;
 	}
 	return (0);
-}
-
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t	i;
-	char	*d;
-	char	*s;
-
-	i = 0;
-	d = (char *)dst;
-	s = (char *)src;
-	if (n == 0 || dst == src)
-		return (dst);
-	while (i < n)
-	{
-		*(d + i) = *(s + i);
-		i++;
-	}
-	return (dst);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*d;
-	size_t	l1;
-	size_t	l2;
-
-	if (s1 == 0 && s2 == 0)
-		return (ft_strdup(""));
-	if (s1 == 0)
-		return (ft_strdup(s2));
-	if (s2 == 0)
-		return (ft_strdup(s1));
-	l1 = ft_strlen (s1);
-	l2 = ft_strlen (s2);
-	d = malloc((l1 + l2 + 1) * sizeof (char));
-	if (d == 0)
-		return (0);
-	ft_memcpy(d, s1, l1);
-	ft_memcpy(d + l1, s2, l2);
-	d[l1 + l2] = '\0';
-	return (d);
 }
